82b05d3a1a1b1f7ecddb196fc7b1939d96db0b19
Shivpal Singh <157144680+RedChargeXT@users.noreply.github.com>
2025-04-09 12:01:25 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(a, b): return a + b
+def subtract(a, b): return a - b
+def multiply(a, b): return a * b
+def divide(a, b): return a / b
+def exponent(a, b): return a ** b
+
+
+
+print(exponent(2, 3))
+print(multiply(2, 3))
+print(add(2, 3))
